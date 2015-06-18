15c54e60cce553499958209956f14b5bceb880a4
zhl11b <zhl11b@live.cn>
2015-06-18 14:21:23 +0800

    Update TwoSum.cpp

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -36,3 +36,13 @@ int *twoSum(int numbers[], int n, int target) {
     }
     return result;
 }
+
+int main()
+{
+    int numbers[] = {0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82};
+
+    int* res = twoSum(numbers, 42, 8);
+    //std::cout<<*res<<" "<<*(++res)<<std::endl;
+    std::cout<<*res;
+    std::cout<<*(++res)<<std::endl;
+}
