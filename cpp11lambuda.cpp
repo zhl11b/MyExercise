3154b2c559fcad85b945251b43772b597888f81a
zhl11b <zhl11b@live.cn>
2015-08-15 22:15:06 +0800

    Update cpp11lambuda.cpp

--- cpp11lambuda.cpp
+++ cpp11lambuda.cpp
@@ -11,14 +11,17 @@ struct StuInfo
 	}
 };
 
-std::vector<StuInfo> pots;
-
-for (int i = 0; i < 10; i++)
+int main()
 {
-	StuInfo pot;
-	pot.level = rand();
-	pots.push_back(pot) ;
-}
+	std::vector<StuInfo> pots;
 
-// 利用lambuda表达式排序
-std::sort(pots.begin(), pots.end(), [](const StuInfo& lpot, const StuInfo& rpot){ return lpot.level<rpot.level;});
+	for (int i = 0; i < 10; i++)
+	{
+		StuInfo pot;
+		pot.level = rand();
+		pots.push_back(pot) ;
+	}
+
+	// 利用lambuda表达式排序
+	std::sort(pots.begin(), pots.end(), [](const StuInfo& lpot, const StuInfo& rpot){ return lpot.level<rpot.level;});
+}
